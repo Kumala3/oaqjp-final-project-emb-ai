8649ae189ae412becd72fc1378a29f00691e4fd8
Kostek <ksytnik73kwer@gmail.com>
2024-05-16 23:02:34 +0200

    Update emotion_detection.py to use JSON data in POST request

--- emotion_detection.py
+++ emotion_detection.py
@@ -5,11 +5,9 @@ def emotion_detector(text_to_analyse: str):
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
 
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-
     data = {"raw_document": {"text": text_to_analyse}}
 
-    response = requests.post(url=url, data=data, headers=headers)
-
+    response = requests.post(url=url, json=data, headers=headers)
     return response.text
 
 
