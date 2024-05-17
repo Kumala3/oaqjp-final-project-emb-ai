79a10d53ee974ee7114604d03456a86dd0256b24
Kostek <ksytnik73kwer@gmail.com>
2024-05-17 21:16:50 +0200

    Implement error handling and checking the presence of text_to_analyze in "/emotionDetector" endpoint

--- server.py
+++ server.py
@@ -12,11 +12,17 @@ def home():
 
 @app.route("/emotionDetector")
 def detect_emotion():
-    text_to_analyze = str(request.args.text_to_analyze)
+    text_to_analyze = str(request.args.textToAnalyze)
 
-    response = emotion_detector(text_to_analyze)
-    format_response = jsonify(emotion_predictor(response))
-    return format_response
+    if not text_to_analyze:
+        return jsonify({"error": "Text is required parameter"}, 422)
+
+    try:
+        response = emotion_detector(text_to_analyze)
+        format_response = jsonify(emotion_predictor(response))
+        return format_response
+    except Exception as e:
+        return jsonify({"error": "Server Error"}, 500)
 
 
 if __name__ == "__main__":
