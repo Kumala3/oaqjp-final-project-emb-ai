58622722404e6914df9527fb56e9e1a597ccb658
Kostek <ksytnik73kwer@gmail.com>
2024-05-17 22:16:52 +0200

    Add docs to functions and endpoints

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -3,6 +3,15 @@ from json import loads, dumps
 
 
 def emotion_detector(text_to_analyse: str):
+    """
+    Detects the emotion in the given text using a remote API.
+
+    Args:
+        text_to_analyse (str): The text to be analyzed for emotion detection.
+
+    Returns:
+        str: The response from the emotion detection API. If the API call is successful, the response will contain the detected emotions. If the API call fails, the response will contain an error message.
+    """
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
 
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
@@ -25,11 +34,20 @@ def emotion_detector(text_to_analyse: str):
             )
         else:
             return dumps({"error": "Server Error"})
-    except Exception as e:
+    except Exception:
         return dumps({"error": "Server Error"})
 
 
-def emotion_predictor(response):
+def emotion_predictor(response: dict):
+    """
+    Predicts the dominant emotion from the given response.
+
+    Args:
+        response (dict): A dictionary containing the response data.
+
+    Returns:
+        dict: A dictionary containing the emotions and the dominant emotion.
+    """
     json_res = loads(response)
     emotions = json_res["emotionPredictions"][0]["emotion"]
 
--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""
+Server module for emotion detection application using Flask.
+"""
+
 from flask import Flask, render_template, jsonify, request
 
 from EmotionDetection.emotion_detection import emotion_detector, emotion_predictor
@@ -7,26 +11,36 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    """
+    Renders the index.html template.
+
+    Returns:
+        The rendered index.html template.
+    """
     return render_template("index.html")
 
 
 @app.route("/emotionDetector")
 def detect_emotion():
+    """
+    Endpoint for detecting emotions in text.
+
+    Returns:
+        A JSON response containing the formatted emotion prediction or an error message.
+
+    """
     text_to_analyze = str(request.args.get("textToAnalyze"))
 
     if not text_to_analyze:
         return jsonify("Invalid text! Please try again!"), 422
 
-    try:
-        response = emotion_detector(text_to_analyze)
-        format_response = emotion_predictor(response)
+    response = emotion_detector(text_to_analyze)
+    format_response = emotion_predictor(response)
 
-        if format_response["dominant_emotion"] is None:
-            return jsonify({"error": "Invalid text! Please try again!."}, 400)
+    if format_response["dominant_emotion"] is None:
+        return jsonify({"error": "Invalid text! Please try again!."}, 400)
 
-        return format_response, 200
-    except Exception as e:
-        return jsonify({"error": "Server Error"}, 500)
+    return format_response, 200
 
 
 if __name__ == "__main__":
