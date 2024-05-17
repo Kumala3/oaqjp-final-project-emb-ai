0a297bce901e7237eb376821a86694c459b7dba7
Kostek <ksytnik73kwer@gmail.com>
2024-05-17 07:43:19 +0200

    Add more unit tests to include sadness, anger, fear, and disgust

--- EmotionDetection/test_emotion_detection.py
+++ EmotionDetection/test_emotion_detection.py
@@ -1,6 +1,7 @@
 import unittest
 
-from EmotionDetection import emotion_predictor, emotion_detector
+from emotion_detection import emotion_predictor, emotion_detector
+
 
 class TestEmotionDetection(unittest.TestCase):
     def test_joy(self):
@@ -8,3 +9,27 @@ class TestEmotionDetection(unittest.TestCase):
         response = emotion_detector(text)
         result = emotion_predictor(response)["dominant_emotion"]
         self.assertEqual(result, "joy")
+
+    def test_sadness(self):
+        text = "I am so sad about this"
+        response = emotion_detector(text)
+        result = emotion_predictor(response)["dominant_emotion"]
+        self.assertEqual(result, "sadness")
+
+    def test_anger(self):
+        text = "I am really mad about this"
+        response = emotion_detector(text)
+        result = emotion_predictor(response)["dominant_emotion"]
+        self.assertEqual(result, "anger")
+
+    def test_fear(self):
+        text = "I am really afraid that this will happen"
+        response = emotion_detector(text)
+        result = emotion_predictor(response)["dominant_emotion"]
+        self.assertEqual(result, "fear")
+
+    def test_disgust(self):
+        text = "I feel disgusted just hearing about this"
+        response = emotion_detector(text)
+        result = emotion_predictor(response)["dominant_emotion"]
+        self.assertEqual(result, "disgust")
