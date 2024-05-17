bef5a6149117f7ebb977ebc13430fe7e6d00d38c
Kostek <ksytnik73kwer@gmail.com>
2024-05-17 21:04:15 +0200

    Implement "/emotionDetector" endpoint and refactor server.py

--- server.py
+++ server.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template, redirect, jsonify
+from flask import Flask, render_template, jsonify, request
+
+from EmotionDetection.emotion_detection import emotion_detector, emotion_predictor
 
 app = Flask(__name__)
 
-@app.router("/emotionDetector")
-def emotion_detector():
+
+@app.route("/")
+def home():
     return render_template("index.html")
 
+
+@app.route("/emotionDetector")
+def detect_emotion():
+    text_to_analyze = str(request.args.text_to_analyze)
+
+    response = emotion_detector(text_to_analyze)
+    format_response = jsonify(emotion_predictor(response))
+    return format_response
 
 
 if __name__ == "__main__":
